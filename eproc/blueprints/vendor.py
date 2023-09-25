e2d596a0d2c535f6deba2236bc6fe79d8b8c8438
JoJo <joeriochandra@gmail.com>
2023-09-25 18:01:00 +0700

    Revert "refactored vendor detail into separate module of resource"
    This reverts commit 618fb73bfa03ad6e330bc6e0777126967ef01ae0.

--- eproc/blueprints/vendor.py
+++ eproc/blueprints/vendor.py
@@ -1,8 +1,10 @@
 from flask import Blueprint
 from flask_restful import Api
 
-from eproc.resources.vendor.vendor import VendorResource
-from eproc.resources.vendor.vendor_detail import VendorDetailResource
+from eproc.resources.vendor.vendor import (
+    VendorDetailResource,
+    VendorResource
+)
 
 vendor_blueprint = Blueprint("vendor_blueprint", __name__, url_prefix="/vendor")
 vendor_api = Api(vendor_blueprint)
--- eproc/resources/vendor/vendor.py
+++ eproc/resources/vendor/vendor.py
@@ -8,6 +8,7 @@ from eproc.controllers.vendor.vendor import VendorController
 from eproc.helpers.commons import split_string_into_list
 from eproc.schemas.vendors.vendors import (
     VendorDeleteInputSchema,
+    VendorDetailGetInputSchema,
     VendorGetInputSchema,
     VendorPostInputSchema,
     VendorPutInputSchema,
@@ -124,3 +125,30 @@ class VendorResource(Resource):
                 HTTPStatus.INTERNAL_SERVER_ERROR
             )
 
+
+class VendorDetailResource(Resource):
+    def get(self) -> Response:
+        try:
+            schema = VendorDetailGetInputSchema()
+
+            is_valid, response, payload = schema_validate_and_load(
+                schema=schema,
+                payload=request.args.to_dict(),
+            )
+            if not is_valid:
+                return response
+
+            (
+                http_status, message, data
+            ) = VendorController().get_detail(payload["id"])
+
+            return make_json_response(
+                http_status=http_status,
+                message=message,
+                data=data
+            )
+        except Exception as e:
+            error_logger.error(f"Error on Vendor Detail [GET] :: {e}, {format_exc()}")
+            return make_json_response(
+                HTTPStatus.INTERNAL_SERVER_ERROR
+            )
