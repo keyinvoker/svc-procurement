3bd162edd373c04341e7009347de607cbbbb2711
JoJo <joeriochandra@gmail.com>
2023-09-27 11:56:08 +0700

    removed `default=None` argument from several fields

--- eproc/models/users/users.py
+++ eproc/models/users/users.py
@@ -10,17 +10,17 @@ class User(BaseModel):
     __tablename__ = "users"
 
     id = sa.Column(sa.String(), primary_key=True)
-    app_id = sa.Column("appid", sa.String(), default=None)
-    username = sa.Column("uname", sa.String(), default=None)  # TODO: delete later, because literally same as `usrid`
-    full_name = sa.Column("ffnam", sa.String(), default=None)
-    password = sa.Column("paswd", sa.String(), default=None)
-    password_length = sa.Column("pasfm", sa.Integer(), default=None)
-    password_salt = sa.Column("passt", sa.String(), default=None)
-    password_hash = sa.Column("pashs", sa.String(), default=None)
-    password_question = sa.Column("pasqs", sa.String(), default=None)
-    password_answer = sa.Column("pasaw", sa.String(), default=None)
-    security_status = sa.Column("secst", sa.String(10), default=None)
-    email = sa.Column("email", sa.String(), default=None)
+    app_id = sa.Column("appid", sa.String())
+    username = sa.Column("uname", sa.String())  # TODO: delete later, because literally same as `usrid`
+    full_name = sa.Column("ffnam", sa.String())
+    password = sa.Column("paswd", sa.String())
+    password_length = sa.Column("pasfm", sa.Integer())
+    password_salt = sa.Column("passt", sa.String())
+    password_hash = sa.Column("pashs", sa.String())
+    password_question = sa.Column("pasqs", sa.String())
+    password_answer = sa.Column("pasaw", sa.String())
+    security_status = sa.Column("secst", sa.String(10))
+    email = sa.Column("email", sa.String())
     emcon = sa.Column("emcon", sa.Integer(), default=0)  # TODO: change to boolean
     is_email_confirmed = column_property(case(
         (emcon == 1, True), else_=False
@@ -53,15 +53,15 @@ class User(BaseModel):
     is_locked = column_property(case(
         (islck == 1, True), else_=False
     ))
-    captcha = sa.Column("sapsa", sa.String(6), default=None)  # TODO: idk wtf for ???
-    mobile_pin = sa.Column("mopin", sa.String(16), default=None)
-    mobile_alias = sa.Column("moals", sa.String(16), default=None)
-    last_active_date = sa.Column("ladat", sa.DateTime(), default=None)
-    last_login_date = sa.Column("llgdt", sa.DateTime(), default=None)
-    last_lock_date = sa.Column("llkdt", sa.DateTime(), default=None)
-    last_password_change_date = sa.Column("lpcdt", sa.DateTime(), default=None)
-    comment = sa.Column("comnt", sa.String(), default=None)
-    phone_number = sa.Column("phono", sa.String(), default=None)
+    captcha = sa.Column("sapsa", sa.String(6))  # TODO: idk wtf for ???
+    mobile_pin = sa.Column("mopin", sa.String(16))
+    mobile_alias = sa.Column("moals", sa.String(16))
+    last_active_date = sa.Column("ladat", sa.DateTime())
+    last_login_date = sa.Column("llgdt", sa.DateTime())
+    last_lock_date = sa.Column("llkdt", sa.DateTime())
+    last_password_change_date = sa.Column("lpcdt", sa.DateTime())
+    comment = sa.Column("comnt", sa.String())
+    phone_number = sa.Column("phono", sa.String())
     phonc = sa.Column("phonc", sa.Integer(), default=0)
     is_phone_number_confirmed = column_property(
         case((phonc == 1, True), else_=False)
@@ -74,19 +74,19 @@ class User(BaseModel):
     lock_enabled = column_property(
         case((locen == 1, True), else_=False)
     )
-    valid_until = sa.Column("loced", sa.DateTime(), default=None)
-    acfct = sa.Column("acfct", sa.Integer(), default=None)
+    valid_until = sa.Column("loced", sa.DateTime())
+    acfct = sa.Column("acfct", sa.Integer())
     remme = sa.Column("remme", sa.Integer(), default=0)  # TODO: change to boolean
     remember_me = column_property(
         case((remme == 1, True), else_=False)
     )
-    first_approver_id = sa.Column("ldid1", sa.String(), default=None)
-    second_approver_id = sa.Column("ldid2", sa.String(), default=None)
-    third_approver_id = sa.Column("ldid3", sa.String(), default=None)
-    flag1 = sa.Column("flag1", sa.String(15), default=None)
-    flag2 = sa.Column("flag2", sa.String(15), default=None)
+    first_approver_id = sa.Column("ldid1", sa.String())
+    second_approver_id = sa.Column("ldid2", sa.String())
+    third_approver_id = sa.Column("ldid3", sa.String())
+    flag1 = sa.Column("flag1", sa.String(15))
+    flag2 = sa.Column("flag2", sa.String(15))
     reference_id = sa.Column("stats", sa.Integer(), sa.ForeignKey("references.id"))
-    temps = sa.Column("temps", sa.String(100), default=None)
+    temps = sa.Column("temps", sa.String(100))
 
     isact = sa.Column("isact", sa.Integer(), default=0)  # TODO: change to boolean
     is_active = column_property(
