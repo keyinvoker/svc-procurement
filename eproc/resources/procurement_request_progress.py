733c0ed7ed7bcb5c808de52e177691aed72854c1
JoJo <joeriochandra@gmail.com>
2023-10-03 11:58:25 +0700

    edited query

--- eproc/resources/procurement_request_progress.py
+++ eproc/resources/procurement_request_progress.py
@@ -11,26 +11,28 @@ from eproc.tools.validation import schema_validate_and_load
 
 
 class ProcurementRequestProgressResource(Resource):
-    def get(self):
+    def get(self) -> Response:
         try:
             raw_query = text("""
-                select a.trnno, a.docno, a.descr,
-                case when b.trnno is null then 'Process' else 'Final' end as temps
-                from procurement_requests as a
-                left join tivty as b on b.dref1 = a.docno
-                order by a.trnno
+                SELECT status, COUNT(*) AS status_count
+                FROM
+                (
+                    SELECT
+                        CASE 
+                            WHEN i.trnno IS NULL THEN 'In Process'
+                            ELSE 'Final'
+                        END AS status
+                    FROM procurement_requests AS pr
+                    LEFT JOIN inventories AS i ON i.dref1 = pr.docno
+                )
+                GROUP BY status;
             """)
 
             results = db.session.execute(raw_query).fetchall()
-            total = len(results)
 
-            in_process = 0
-            final = 0
-            for result in results:
-                if result[3] == "Final":
-                    final += 1
-                elif result[3] == "Process":
-                    in_process += 1
+            final = results[0].status_count
+            in_process = results[1].status_count
+            total = in_process + final
             
             in_process_percentage = round((in_process / total * 100), 2)
             final_percentage = round((final / total * 100), 2)
