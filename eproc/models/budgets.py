7cc9ffe2798502891517531c36a9c33ad643f27e
JoJo <joeriochandra@gmail.com>
2023-10-16 15:48:43 +0700

    removed extra whitespace

--- eproc/models/budgets.py
+++ eproc/models/budgets.py
@@ -8,7 +8,6 @@ from eproc.models.base_model import BaseModel
 class Budget(BaseModel):
     __tablename__ = "budgets"
 
-
     cost_center_id = sa.Column(sa.String(20), sa.ForeignKey("cost_centers.id"), primary_key=True)
     year = sa.Column(sa.Integer(), primary_key=True)
     upload_count = sa.Column(sa.Integer(), primary_key=True, autoincrement=True)
