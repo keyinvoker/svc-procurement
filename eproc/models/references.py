ff31d7f3f27fcac0ecf3cb9f33e3c178cd3a5824
JoJo <joeriochandra@gmail.com>
2023-10-21 16:31:49 +0700

    updated references table with correct field names

--- eproc/models/references.py
+++ eproc/models/references.py
@@ -1,8 +1,5 @@
 import sqlalchemy as sa
-from sqlalchemy.orm import backref, column_property
-from sqlalchemy.sql import case
 
-from eproc import db
 from eproc.models.base_model import BaseModel
 
 
@@ -10,17 +7,13 @@ class Reference(BaseModel):
     __tablename__ = "references"
 
     id = sa.Column(sa.Integer(), primary_key=True)
-    refid = sa.Column("refid", sa.String(20), nullable=False)  # TODO: Foreign Key to what table?
-    cdval = sa.Column("cdval", sa.Numeric(18, 2), nullable=False)
-    cdtxt = sa.Column("cdtxt", sa.String(100))
-    description = sa.Column("descr", sa.String(1000), nullable=False)
-    zordr = sa.Column("zordr", sa.BigInteger(), nullable=False)
+    refid = sa.Column("refid", sa.String(20), nullable=False)
+    parameter_value = sa.Column(sa.Numeric(18, 2), nullable=False)
+    parameter_text = sa.Column(sa.String(100))
+    description = sa.Column(sa.String(1000), nullable=False)
+    order_number = sa.Column(sa.BigInteger(), nullable=False)
     rsign = sa.Column("rsign", sa.String(12))
     dref1 = sa.Column(sa.String(20))
     dref2 = sa.Column(sa.String(200))
     dref3 = sa.Column(sa.String(20))
-
-    isact = sa.Column("isact", sa.Integer(), default=1)  # TODO: change to boolean
-    is_active = column_property(
-        case((isact == 1, True), else_=False)
-    )
+    is_active = sa.Column(sa.Boolean(), default=True, server_default="true")
